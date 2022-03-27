f1d660a7846081b5ffa9ccdfa246c9cd36ddbd6d
Curvature Research <gongyuanhao@gmail.com>
2022-03-27 10:24:56 +0800

    Update WeightedMeanCurvature.m
    Neumann boundary condition

--- WeightedMeanCurvature.m
+++ WeightedMeanCurvature.m
@@ -1,16 +1,17 @@
 function Hw=WeightedMeanCurvature(u)
 %compute weighted mean curvature
+u=padarray(u,[1,1],'replicate'); 
 k1=[1,1,0;  2, -6,0;  1,1,0]/6;
 k2=[2,4,1;  4,-12,0;  1,0,0]/12;
-dist = zeros([size(u),8],'single');
-dist(:,:,1) = conv2(u,k1,'same');
-dist(:,:,2) = conv2(u,fliplr(k1),'same');
-dist(:,:,3) = conv2(u,k1','same');
-dist(:,:,4) = conv2(u,flipud(k1'),'same');
-dist(:,:,5) = conv2(u,k2,'same');
-dist(:,:,6) = conv2(u,fliplr(k2),'same');
-dist(:,:,7) = conv2(u,flipud(k2),'same');
-dist(:,:,8) = conv2(u,rot90(k2,2),'same');
+dist = zeros([size(u)-2,8],'single');
+dist(:,:,1) = conv2(u,k,'valid');
+dist(:,:,2) = conv2(u,fliplr(k),'valid');
+dist(:,:,3) = conv2(u,k','valid');
+dist(:,:,4) = conv2(u,flipud(k'),'valid');
+dist(:,:,5) = conv2(u,k2,'valid');
+dist(:,:,6) = conv2(u,fliplr(k2),'valid');
+dist(:,:,7) = conv2(u,flipud(k2),'valid');
+dist(:,:,8) = conv2(u,rot90(k2,2),'valid');
 %% find minimum signed distance
 [~,ind] = min(abs(dist),[],3);
 %turn sub to index (sub2ind but faster)
